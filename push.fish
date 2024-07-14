8f1f467fe8fa93bf9b840402fa9bcc2e30afad8d
Aramís Concepción Durán <aramis@systemli.org>
2024-07-14 08:28:40 +0200

    Von Elm auf Gren umwurschteln
    push.fish
    
    gren.json

---
+++ push.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+git push origin
+git push git@git.sr.ht:~aramis/gren-keyboard-event
+git push git@github.com:aramiscd/gren-keyboard-event
+
+git push --tags origin
+git push --tags git@git.sr.ht:~aramis/gren-keyboard-event
+git push --tags git@github.com:aramiscd/gren-keyboard-event
--- tests/compile.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-mkdir -p build/Keyboard
-elm make src/Keyboard/Event.elm --output build/Keyboard/Event.js
-cd examples
-elm make OutermostDiv.elm --output ../build/OutermostDiv.html
-elm make TwoDivs.elm --output ../build/TwoDivs.html
-elm make Document.elm --output ../build/Document.html
